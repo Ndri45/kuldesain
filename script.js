351a47b58ea9714f4c004baf80cf80b4684685ed
Ndri45 <as.ndr82@gmail.com>
2020-08-17 15:27:41 +0700

    add div#dim

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-let scrollTimer = null;
-window.onscroll = function() {
-    document.querySelector('.fixed-top').style.top = '0px';
-    document.querySelector('.fixed-top').style.transition = 'top 3s';
-    if (scrollTimer != null) clearTimeout(scrollTimer);
-    scrollTimer = window.setTimeout("scrollFinished()", 500);
-};
+    let scrollTimer = null;
+    window.onscroll = function() {
+        document.querySelector('#dim').style.display = 'block';
+        document.querySelector('#dim').style.display = 'top 3s';
+        if (document.body.clientWidth >= 576) {
+            document.querySelector('.fixed-top').style.top = '0px';
+            document.querySelector('.fixed-top').style.transition = 'top 3s';
+        }
+        if (scrollTimer != null) clearTimeout(scrollTimer);
+        scrollTimer = window.setTimeout("scrollFinished()", 500);
+    }
 
-function scrollFinished() {
-    document.querySelector('.fixed-top').style.top = '-60px';
-    document.querySelector('.fixed-top').style.transition = 'top 3s';
-}
+    function scrollFinished() {
+        document.querySelector('#dim').style.display = 'none';
+        document.querySelector('#dim').style.display = 'top 3s';
+        if (document.body.clientWidth >= 576) {
+            document.querySelector('.fixed-top').style.top = '-60px';
+            document.querySelector('.fixed-top').style.transition = 'top 3s';
+        }
+    }
